7bfc0530fcc26a5c0dbc87d7d38585b5a58c608b
Jon Arryn <development@arryn.net>
2024-07-31 07:56:22 -0400

    adds mysql package, language version target, and renames main view

--- AppointmentApp/Program.cs
+++ AppointmentApp/Program.cs
@@ -16,7 +16,7 @@ namespace AppointmentApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainView());
         }
     }
 }
